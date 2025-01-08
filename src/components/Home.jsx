817d9316b82f3e9f1f7ed2768faf38b270b77dac
gabymcanales <gabycanales728@gmail.com>
2025-01-07 22:35:18 -0600

    inicio de diseños

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -24,16 +24,66 @@ export default function Home() {
 
   return (
     <div> 
+    
       {
         user ? (
           <>
           <header>
             <Navegacion/>
           </header>
-      <main>
-        {/* Contenido principal */}
-        <h1 className='mt-5'>BIENVENIDOS A LA APP DE ALOJAMIENTOS Y RESERVACIONES</h1>
-      </main>
+
+          <main className={styles.hero}>
+          {/* Contenido principal */}
+          <h1 className={`mt-5 ${styles.overlay}`}>BIENVENIDOS A LA APP DE ALOJAMIENTOS Y RESERVACIONES</h1>
+           </main>
+          
+      
+      {/* Destinations */}
+      <section className="container destinations py-5">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card">
+              <img
+                src="https://res.cloudinary.com/dqo8icjui/image/upload/v1736293282/alojamientos/m7ldzromeovb4tssh3px.webp"
+                className="card-img-top"
+                alt="Hawaii"
+              />
+              <div className="card-body">
+                <h5 className="card-title">Surf the waves of Hawaii</h5>
+                <p className="card-text">Pay monthly from £74.07</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <img
+                src="https://source.unsplash.com/400x300/?beach"
+                className="card-img-top"
+                alt="Bora Bora"
+              />
+              <div className="card-body">
+                <h5 className="card-title">Paradise vacation</h5>
+                <p className="card-text">Pay monthly from £74.07</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <img
+                src="https://source.unsplash.com/400x300/?island"
+                className="card-img-top"
+                alt="Fiji"
+              />
+              <div className="card-body">
+                <h5 className="card-title">Explore Fiji's fantasy coast</h5>
+                <p className="card-text">Pay monthly from £74.07</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+         
           </>
         ) : ( <h2>Redirigiendo...</h2>)
       }
