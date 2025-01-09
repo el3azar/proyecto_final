4bce3cd0b5b98763e16981e910466aa0b691d616
gabymcanales <gabycanales728@gmail.com>
2025-01-08 19:56:34 -0600

    mejorando login

--- src/components/Navegacion.jsx
+++ src/components/Navegacion.jsx
@@ -8,23 +8,22 @@ import styles from '../styles/Navegacion.module.css';
 
 export default function () {
   return (
-    <div>
         <nav className={`navbar navbar-expand-lg ${styles.navbar}`}>
                   <section className="container-fluid">
                                        
                     <div className={`collapse navbar-collapse `} id="navbarSupportedContent">
-                      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
+                      <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-4">
+                        <li className="nav-item ">
                           <Link to="/home" className={`nav-link active ${styles['nav-link']}`} aria-current="page" >Inicio</Link>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item ms-2">
                         <Link to="/alojamientos" className={`nav-link active ${styles['nav-link']}`} aria-current="page" >Alojamientos</Link>
                         </li>
                         
-                        <li className="nav-item">
+                        <li className="nav-item ms-2 ">
                         <Link to="/reservaciones" className={`nav-link active ${styles['nav-link']}`} aria-current="page" >Reservaciones</Link>
                         </li>
-                        <li className="nav-item">
+                        <li className="nav-item ms-2">
                         <Link to="/calendario" className={`nav-link active ${styles['nav-link']}`} aria-current="page" >Calendario</Link>
                         </li>
                       </ul>
@@ -34,6 +33,7 @@ export default function () {
                     </div>
                   </section>
         </nav>
-  </div>
+
+  
   )
 }
